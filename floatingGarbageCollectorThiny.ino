b9bf64348d52ee267856d4f79839110ce30fe001
MyAngelShibuyaKanon <99974252+MyAngelShibuyaKanon@users.noreply.github.com>
2026-04-20 20:05:11 +0800

    update to 1 layer as requested

--- floatingGarbageCollectorThiny.ino
+++ floatingGarbageCollectorThiny.ino
@@ -5,8 +5,6 @@
 #define MIN_DISTANCE_VOLT 2100 // 2100 millivolt, check sensor distance to volt graph to adjust
 //GPIO Pins, check diagram, ADC pins
 #define IR_PIN1 34
-#define IR_PIN2 32
-#define IR_PIN3 33
 
 // water level sensor switch
 # define WATER_SWITCH_PIN 27
@@ -39,32 +37,27 @@ void loop() {
   // put your main code here, to run repeatedly:
   // get voltage readings on IR sensors
   int dist1 = analogReadMilliVolts(IR_PIN1);
-  int dist2 = analogReadMilliVolts(IR_PIN2);
-  int dist3 = analogReadMilliVolts(IR_PIN3);
-
 
   bool layer1Full = (dist1 >= MIN_DISTANCE_VOLT);
-  bool layer2Full = (dist2 >= MIN_DISTANCE_VOLT);
-  bool layer3Full = (dist3 >= MIN_DISTANCE_VOLT);
 
   // check if water switch active, if so, it means the water level is high
   bool waterHigh = !digitalRead(WATER_SWITCH_PIN);
 
-  // combined boolean logic for 3 IR sensor
-  bool isBinFull = layer1Full && layer2Full && layer3Full;
+  // combined boolean logic for 3 IR sensor (updated to 1)
+  bool isBinFull = layer1Full;
 
   if (isBinFull) {
     stopSystem("Bin Full!");
   } else if (waterHigh){
     stopSystem("Water High!");
   }else {
-    runSystem(layer1Full, layer2Full, layer3Full);
+    runSystem();
   }
 
   delay(500);
 }
 
-void runSystem(bool layer1Full, bool layer2Full, bool layer3Full) {
+void runSystem() {
   // 1st line of lcd: main status
   // 2nd line of lcd: layer status 
   digitalWrite(PUMP_RELAY_PIN, HIGH);
@@ -73,12 +66,7 @@ void runSystem(bool layer1Full, bool layer2Full, bool layer3Full) {
   lcd.setCursor(0, 0);
   lcd.print("System Running");
   lcd.setCursor(0, 1);
-  char buffer[17]; // Buffer for 16 characters + null terminator
-  snprintf(buffer, sizeof(buffer), "L1:%c L2:%c L3:%c", 
-           layer1Full ? 'F' : 'O', 
-           layer2Full ? 'F' : 'O', 
-           layer3Full ? 'F' : 'O');
-  lcd.print(buffer);
+  lcd.print("Bin OK");
 }
 
 void stopSystem(const char* message) {
